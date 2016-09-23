c60386b14b0b3e5f55198b042f3558370af5eb91
pangye <pangye@192.168.2.50>
2016-09-24 01:08:29 +0800

    Initial Commit

---
+++ gwmDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gwmDemo
+//
+//  Created by pangye on 16/9/24.
+//  Copyright © 2016年 pangye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ gwmDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  gwmDemo
+//
+//  Created by pangye on 16/9/24.
+//  Copyright © 2016年 pangye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
